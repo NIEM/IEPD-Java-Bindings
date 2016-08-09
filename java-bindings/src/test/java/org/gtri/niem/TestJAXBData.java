607c21fee5bf002eebe7c31ccba8ae5e2c113a0e
Brad Lee <s.brad.lee@gmail.com>
2016-08-09 15:36:29 -0400

    Did some renaming and in general moving stuff around. DOES NOT BUILD.

---
+++ java-bindings/src/test/java/org/gtri/niem/TestJAXBData.java
@@ -0,0 +1,17 @@
+package org.gtri.niem;
+
+import org.junit.Test;
+
+/**
+ * A parameterized test, which finds any XML instances from "iepd-source" and runs them end-to-end using JAXB.
+ * <br/><br/>
+ * Created by brad on 8/9/16.
+ */
+public class TestJAXBData {
+
+    @Test
+    public void runTest(){
+
+    }
+
+}
