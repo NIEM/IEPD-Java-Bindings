c6e9d79af68449a9cdd0e2aec73ba92e50f4a90d
Brad Lee <s.brad.lee@gmail.com>
2016-08-12 15:16:13 -0400

    Improved testing, added method to get 'intereseting' (ie, non niem) classes from JAXB

--- java-bindings/src/test/java/org/gtri/niem/TestJAXBData.java
+++ java-bindings/src/test/java/org/gtri/niem/TestJAXBData.java
@@ -9,12 +9,11 @@ import org.junit.runners.Parameterized;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.*;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -100,21 +99,48 @@ public class TestJAXBData {
     public void runTest() throws Exception {
         logger.info("Executing JAXB test on XML File["+this.xmlFile.getCanonicalPath()+"]...");
 
-        logger.debug("Creating unmarshaller...");
+        logger.debug("Creating marshaller/unmarshaller...");
         Unmarshaller unmarshaller = JaxbHelper.createUnmarshaller();
         assertThat(unmarshaller, notNullValue());
+        Marshaller marshaller = JaxbHelper.createMarshaller();
+        assertThat(marshaller, notNullValue());
 
         logger.debug("Unmarshalling "+this.xmlFile);
         Object obj = unmarshaller.unmarshal(this.xmlFile);
         assertThat(obj, notNullValue());
 
+        Class clz1 = obj.getClass();
         logger.info("Successfully unmarshalled "+obj.getClass().getName());
         if( obj instanceof JAXBElement ){
             JAXBElement jaxbe = (JAXBElement) obj;
             Object value = jaxbe.getValue();
             assertThat(value, notNullValue());
             logger.info("  JAXBElement is of type: "+value.getClass().getName());
+            clz1 = value.getClass();
         }
+
+        File marshallToFile = new File("./target/test/xml-instances/"+this.xmlFile.getName());
+        marshallToFile.getParentFile().mkdirs();
+        FileWriter fileOut = new FileWriter(marshallToFile, false);
+        marshaller.marshal(obj, fileOut);
+        logger.debug("Successfully marshalled file: "+marshallToFile.getCanonicalPath());
+
+        logger.debug("Unmarshalling the marshalled out file: "+marshallToFile.getCanonicalPath());
+        Object obj2 = unmarshaller.unmarshal(marshallToFile);
+        assertThat(obj2, notNullValue());
+
+        Class clz2 = obj2.getClass();
+        logger.info("Successfully unmarshalled "+obj2.getClass().getName());
+        if( obj2 instanceof JAXBElement ){
+            JAXBElement jaxbe = (JAXBElement) obj2;
+            Object value = jaxbe.getValue();
+            assertThat(value, notNullValue());
+            logger.info("  JAXBElement is of type: "+value.getClass().getName());
+            clz2 = value.getClass();
+        }
+
+        // TODO we could somehow generically compare the two which would do more than this does...
+        assertThat(clz1, equalTo(clz2));
 
         logger.info("Successfully tested JAXB can unmarshall file "+this.xmlFile.getCanonicalPath());
     }
